c388f34b32a90b9ba936b391e2df1d2941cceaf8
Thirumal <m.thirumal@hotmail.com>
2024-02-28 11:08:25 +0530

    First commit

---
+++ src/main/java/com/thirumal/Oauth2BffApplication.java
@@ -0,0 +1,13 @@
+package com.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2BffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2BffApplication.class, args);
+	}
+
+}
